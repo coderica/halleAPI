ef0ef660c26302e27a3badb36d58ca0ef941cb62
coderica <coderica@users.noreply.github.com>
2015-06-01 15:16:31 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users do
     get 'identify', on: :member
   end
+  
+  get '/data', to: 'application#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
